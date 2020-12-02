c495e94f273fa33db9d8eccf9f3312e6a9348eb7
ManuelP <manuepeva@gmail.com>
2020-12-02 08:13:32 -0500

    Creating folder structure on frontend and defining styles for each of them

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import makeStyles from './styles'
+
+const Form = () => {
+  const classes = makeStyles()
+    return (
+        <div>
+          Form  
+        </div>
+    )
+}
+
+export default Form
---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import makeStyles from './styles'
+
+const Post = () => {
+    const classes = makeStyles()
+    return (
+        <div>
+            Post
+        </div>
+    )
+}
+
+export default Post
